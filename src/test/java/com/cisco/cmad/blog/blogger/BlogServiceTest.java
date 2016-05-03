2fcf6450ab5527fef0ff4107c9227995f5c3970a
Krish <ynkrish@gmail.com>
2016-05-03 20:47:41 +0530

    Updated Dockerfile and test case

--- src/test/java/com/cisco/cmad/blog/blogger/BlogServiceTest.java
+++ src/test/java/com/cisco/cmad/blog/blogger/BlogServiceTest.java
@@ -2,6 +2,7 @@ package com.cisco.cmad.blog.blogger;
 
 import com.cisco.cmad.blog.blogger.dao.BlogDAO;
 import com.cisco.cmad.blog.blogger.model.Blog;
+import com.cisco.cmad.blog.blogger.model.Comment;
 import com.cisco.cmad.blog.blogger.service.BlogService;
 import com.cisco.cmad.blog.blogger.service.BlogServiceImpl;
 import org.bson.types.ObjectId;
@@ -13,35 +14,46 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.mongodb.morphia.query.Query;
-import org.mongodb.morphia.query.QueryResults;
+import org.mongodb.morphia.Key;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 import static java.lang.System.out;
 
 /**
  * Created by kyechcha on 03-May-16.
+ *
+ * Unit test case for CompanyService
  */
 @RunWith(MockitoJUnitRunner.class)
 public class BlogServiceTest {
 
     @Mock private BlogDAO blogDaoMock;
 
-    @Mock private Query<Blog> blogQueryMock;
+    @Mock private Key<Blog> blogKey;
 
     @InjectMocks private BlogService blogService = new BlogServiceImpl(blogDaoMock);
 
+
+    private Blog getBlog(String title, Optional<String> tag) {
+        Blog blog = Mockito.mock(Blog.class);
+        Mockito.when(blog.getId()).thenReturn(ObjectId.get().toHexString());
+        Mockito.when(blog.getTitle()).thenReturn(title);
+        Mockito.when(blog.getTags()).thenReturn(tag.orElse(""));
+        return blog;
+    }
+
+    /**
+     * Try to fetch all blogs
+     */
     @Test
     public void testBlogList() {
 
         List<Blog> blogList = new ArrayList<>();
-
-        Blog blog = Mockito.mock(Blog.class);
-        Mockito.when(blog.getId()).thenReturn(ObjectId.get().toHexString());
-        Mockito.when(blog.getTitle()).thenReturn("Dummy Title");
-        blogList.add(blog);
+        blogList.add(getBlog("Dummy Title", Optional.empty()));
+        blogList.add(getBlog("Dummy Title #2", Optional.empty()));
 
         blogList.forEach(b -> out.println("Blog ::" + b.getTitle()));
 
@@ -51,10 +63,110 @@ public class BlogServiceTest {
 
         allBlogList.forEach(b -> out.println("AllBlogList ::" + b));
 
-        Assert.assertEquals("Blog size should match ", 1, allBlogList.size());
-        Assert.assertEquals("Blog titles shoudl match ", allBlogList.get(0).getTitle(), blogList.get(0).getTitle());
+        Assert.assertEquals("Blog size should match ", 2, allBlogList.size());
+        Assert.assertEquals("Blog titles should match ", allBlogList.get(0).getTitle(), blogList.get(0).getTitle());
+    }
+
+    /**
+     * Try to fetch blogs using keyword
+     */
+    @Test
+    public void testBlogListUsingKeyword() {
+        List<Blog> blogList = new ArrayList<>();
+        blogList.add(getBlog("Dummy Title #2", Optional.ofNullable("tag1, tag2")));
+        blogList.add(getBlog("Title #2", Optional.ofNullable("tag3, tag4")));
+
+        BDDMockito.given(blogDaoMock.getBlogs(Optional.ofNullable("tag1"))).willReturn(blogList.subList(0,1));
+
+        List<Blog> taggedBlogList = blogService.getBlogs(Optional.ofNullable("tag1"));
+
+        Assert.assertEquals("Blog size should match ", 1, taggedBlogList.size());
+        Assert.assertEquals("Blog titles should match ", taggedBlogList.get(0).getTitle(), blogList.get(0).getTitle());
+    }
+
+    /**
+     * Try to fetch blogs using a tag that does not exist.
+     */
+    @Test
+    public void testBlogListUsingMissingKeyword() {
+        List<Blog> blogList = new ArrayList<>();
+
+        blogList.add(getBlog("Dummy Title", Optional.ofNullable("tag1, tag2")));
+
+        BDDMockito.given(blogDaoMock.getBlogs(Optional.ofNullable("tag1"))).willReturn(blogList);
+        BDDMockito.given(blogDaoMock.getBlogs(Optional.ofNullable("tag2"))).willReturn(blogList);
+
+        List<Blog> taggedBlogList = blogService.getBlogs(Optional.ofNullable("tag3"));
+
+        Assert.assertEquals("Blog size should match ", 0, taggedBlogList.size());
+    }
+
+    /**
+     * Test that on saving a Blog, an identifier representing the blog is
+     * returned
+     */
+    @Test
+    public void testStoreBlog() {
+
+        Blog blog = getBlog("Test Title", Optional.ofNullable("tag1, tag2"));
+
+        String objId = (new ObjectId()).toHexString();
+
+        BDDMockito.given(blogDaoMock.save(blog)).willReturn(blogKey);
+        BDDMockito.given(blogKey.getId()).willReturn(objId);
+
+        String retVal = blogService.storeBlog(blog);
+
+        Assert.assertEquals("ObjectId should be returned when Blog is saved ", retVal, objId);
 
     }
 
+    /**
+     * Test to ensure that the blog that is saved can be retrieved back post saving
+     */
+    @Test
+    public void testStoreAndRetrieveBlog() {
 
+        List<Blog> blogList = new ArrayList<>();
+        Blog blog = getBlog("Dummy Title", Optional.ofNullable("tag1, tag2"));
+        blogList.add(blog);
+
+        String objId = (new ObjectId()).toHexString();
+        BDDMockito.given(blogDaoMock.save(blog)).willReturn(blogKey);
+        BDDMockito.given(blogKey.getId()).willReturn(objId);
+        BDDMockito.given(blogDaoMock.getBlogs(Optional.empty())).willReturn(blogList);
+
+        blogService.storeBlog(blog);
+
+        List<Blog> allBlogList = blogService.getBlogs(Optional.empty());
+
+        Assert.assertEquals("Blog size should match ", 1, allBlogList.size());
+    }
+
+    @Test
+    public void testUpdateBlogWithComments() {
+
+        String firstName = "Krishnan";
+        String lastName = "Y";
+
+        List<Comment> commentList = new ArrayList<>();
+
+        Comment comment = Mockito.mock(Comment.class);
+        Mockito.when(comment.getContent()).thenReturn("Comment #1");
+        commentList.add(comment);
+
+        List<Blog> blogList = new ArrayList<>();
+        Blog blog = getBlog("Photography", Optional.ofNullable("macro, dslr"));
+
+        Mockito.when(blog.getComments()).thenReturn(commentList);
+        blogList.add(blog);
+
+        BDDMockito.given(blogDaoMock.getBlogs(Optional.empty())).willReturn(blogList);
+
+        List<Blog> allBlogList = blogService.getBlogs(Optional.empty());
+
+        Assert.assertEquals("Comment saved should be same when retrieved ", allBlogList.get(0).getComments(), blogList.get(0).getComments());
+        Assert.assertEquals("Blog size should match ", 1, allBlogList.size());
+
+    }
 }
--- src/test/java/com/cisco/cmad/blog/blogger/BloggerVerticleTest.java
+++ src/test/java/com/cisco/cmad/blog/blogger/BloggerVerticleTest.java
@@ -1,12 +1,15 @@
 package com.cisco.cmad.blog.blogger;
 
+import com.cisco.cmad.blog.blogger.util.HttpResponseCode;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClientOptions;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,40 +23,35 @@ public class BloggerVerticleTest {
 	private Vertx vertx;
 
 	@Before
-	public void setUp(TestContext context) throws Exception {
-		/*vertx = Vertx.vertx();
+	public void before(TestContext context) throws Exception {
+		vertx = Vertx.vertx();
 
 		//Find free port and use that in our code for deploying verticle
 		ServerSocket socket = new ServerSocket(0);
 		port = socket.getLocalPort();
 		socket.close();
+
 		DeploymentOptions options = new DeploymentOptions().setConfig(new JsonObject().put("https.port", port));
-
-		//vertx.deployVerticle(BloggerVerticle.class.getName(), options, context.asyncAssertSuccess()); */
+		vertx.deployVerticle(BloggerVerticle.class.getName(), options, context.asyncAssertSuccess());
 	}
 
-	@After
-	public void tearDown(TestContext context) {
-		//vertx.close(context.asyncAssertSuccess());
-	}
+    @After
+    public void after(TestContext context) {
+        vertx.close(context.asyncAssertSuccess());
+    }
 
 	@Test
-	public void testMyApplication(TestContext context) {
+	public void testHomePageFetch(TestContext context) {
 		
-	/*	final Async async = context.async();
+		final Async async = context.async();
 
-		vertx.createHttpClient().getNow(port, "localhost", "/", response -> {
-			response.handler(body -> {
-				System.out.println("Body :: " + body.toString());
-				context.assertTrue(true);
-				async.complete();
-			});
-		});
+        vertx.createHttpClient(new HttpClientOptions().setSsl(true).setTrustAll(true).setVerifyHost(false)).getNow(port, "localhost", "/", resp -> {
 
-		// NOTE: Without this close, the execution does not end and test fails
-		// due to TimeoutException
-		// Not sure if this is correct given that tearDown is also doing a close
-		vertx.close();*/
+            Assert.assertEquals("Status code should be 200 ", HttpResponseCode.OK.get(), resp.statusCode());
+            resp.bodyHandler(body -> {
+                Assert.assertTrue("Body should contain mysocial tag", body.toString().contains("mysocial"));
+                async.complete();
+            });
+        });
 	}
-
 }
